eafd7542b30167ee8984278304502d2fbf13af64
denis.woeffray <denis.woeffray@gmail.com>
2018-05-30 13:53:26 +0200

    Save responses and navigate between questions.

--- routes/test_mhavie.js
+++ routes/test_mhavie.js
@@ -5,30 +5,63 @@ var models = require('../models');
 router.get('/:code', function (req, res, next) {
     var code = req.params.code;
     models.Questionnaire.findOne({where: {code: code}}).then(function (questionnaire) {
-        if(questionnaire === null)
-        {
-            res.render('index', {title: 'Test', msg: "Le test "+code+" n'existe pas"})
+        if (questionnaire === null) {
+            res.render('index', {title: 'Test', msg: "Le test " + code + " n'existe pas"})
         }
         questionnaire = questionnaire.dataValues;
-        models.Question.findOne({where:{id:questionnaire.last_question}, include: [models.Categorie]}).then(function (question) {
+        models.Question.findOne({
+            where: {number: questionnaire.last_question},
+            include: [models.Categorie]
+        }).then(function (question) {
 
-            res.render('test_mhavie_questions', {title: 'Test', code: code, question:question});
+            //TODO récupérer la réponse si elle existe et l'afficher dans la vue test_mhavie_questions
+            res.render('test_mhavie_questions', {title: 'Test', code: code, question: question});
 
         })
     })
 });
 
+
+router.post('/:code/:question', function (req, res, next) {
+    var code = req.params.code;
+    var questionNb = req.params.question;
+    var next = req.body.next;
+    var value = req.body.valueInput;
+    var satisfaction = req.body.satisfactionInput;
+
+    models.Questionnaire.findOne({where: {code: code}}).then(function (questionnaire) {
+        questionnaire.last_question = Number(questionNb) + Number(next);
+        questionnaire.save({fields: ['last_question']});
+        models.Question.findOne({where: {number: questionNb}}).then(function (question) {
+            models.Reponse.findOrCreate({
+                where: {
+                    questionnaire_id: questionnaire.id,
+                    question_id: question.id
+                }
+            }).spread(function (response, created) {
+                response.value = value;
+                response.satisfaction = satisfaction;
+                response.save({fields: ['value', 'satisfaction']}).then(function (e) {
+                    res.redirect('/test/' + code);
+                })
+            })
+
+        })
+
+    });
+});
 router.post('/', function (req, res, next) {
     var code = generateCode();
     models.Questionnaire.create({
         date: new Date(),
         code: code
     }).then(function (item) {
-        res.redirect('/test/'+code);
+        res.redirect('/test/' + code);
     }).catch(function (err) {
         res.redirect(307, '/test');
     });
 });
+
 
 module.exports = router;
 
