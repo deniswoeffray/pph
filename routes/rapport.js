1680ca1071878c3fbe90bf5698b08b65435694af
deniswoeffray <denis.woeffray@gmail.com>
2018-06-02 14:27:16 +0200

    graphiques pas mal avancés

--- routes/rapport.js
+++ routes/rapport.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/:code', function(req, res, next) {
+  var code = req.params.code;
+
+  //find questionnaire
+    models.Questionnaire.findOne({where:{code:code}}).then(function (questionnaire) {
+
+        models.Reponse.findAll({where:{questionnaire_id:questionnaire.id}, include:[{model:models.Question, include:[{model:models.Categorie}]}]}).then(function (reponses) {
+            res.render('rapport', {code:questionnaire.code, reponses:reponses});
+          })
+        });
+    });
 
 module.exports = router;
